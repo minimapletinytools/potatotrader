f6b7e3000536fe92e1b13bf4d596c36087960dc2
pddla <chippermonky@gmail.com>
2020-01-11 21:57:19 -0800

    added module file stuff

---
+++ src/Potato/CryptoTrader.hs
@@ -0,0 +1,20 @@
+{-|
+Module      : Potato.CryptoTrader
+Description :
+Copyright   : (c) pdlla, 2020
+License     : GPL-3
+Maintainer  : chippermonky@gmail.com
+Stability   : experimental
+-}
+
+module Potato.CryptoTrader (
+  module Potato.CryptoTrader
+  , module Potato.CryptoTrader.Arbitrage
+  , module Potato.CryptoTrader.ReverseExchangePair
+  , module Potato.CryptoTrader.MarketMaker
+) where
+
+import           Potato.CryptoTrader.Arbitrage
+import           Potato.CryptoTrader.MarketMaker
+import           Potato.CryptoTrader.ReverseExchangePair
+import           Potato.CryptoTrader.Types
---
+++ src/Potato/CryptoTrader/Exchanges/Bilaxy.hs
@@ -0,0 +1,14 @@
+{-|
+Module      : Potato.CryptoTrader.Exchanges.Bilaxy
+Description :
+Copyright   : (c) pdlla, 2020
+License     : GPL-3
+Maintainer  : chippermonky@gmail.com
+Stability   : experimental
+-}
+
+module Potato.CryptoTrader.Exchanges.Bilaxy (
+  module Potato.CryptoTrader.Exchanges.Bilaxy.Exchange
+) where
+
+import           Potato.CryptoTrader.Exchanges.Bilaxy.Exchange
